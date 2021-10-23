eceaad2bd36a63687b5ac46e0b3ea5b45a979315
lodi <lodi.dev@outlook.com>
2021-10-23 14:48:16 -0500

    fix error

--- src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
+++ src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
@@ -8,7 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class FavoritesPageComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    console.log('w');
-  }
+  ngOnInit(): void {}
 }
