66652dd4b5619b9a9ee969586825b167e4c300d0
lodi <lodi.dev@outlook.com>
2021-10-23 15:23:57 -0500

    dato by contex

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../data/tracks.json';
 
 @Component({
   selector: 'app-play-list-body',
   templateUrl: './play-list-body.component.html',
-  styleUrls: ['./play-list-body.component.scss']
+  styleUrls: ['./play-list-body.component.scss'],
 })
 export class PlayListBodyComponent implements OnInit {
-
-  constructor() { }
+  tracks: TrackModel[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default;
+    //se manda al componente
+    this.tracks = data;
   }
-
 }
